113a5416f1c9aab8c721cdd9c061e95b6e27177a
carono <info@carono.ru>
2017-12-20 10:06:04 +0500

    check form method

--- src/ActiveFormBehavior.php
+++ src/ActiveFormBehavior.php
@@ -7,7 +7,14 @@ namespace carono\checksum;
 use yii\base\Behavior;
 use yii\base\Widget;
 use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 
+/**
+ * Class ActiveFormBehavior
+ *
+ * @package carono\checksum
+ * @property ActiveForm $owner
+ */
 class ActiveFormBehavior extends Behavior
 {
     public $_checksumInit = true;
@@ -21,7 +28,7 @@ class ActiveFormBehavior extends Behavior
 
     public function registerChecksumField()
     {
-        if (\Yii::$app->request instanceof Request && \Yii::$app->request->checksumIsEnabled()) {
+        if (strtolower($this->owner->method) == 'post' && \Yii::$app->request instanceof Request && \Yii::$app->request->checksumIsEnabled()) {
             $stack = \Yii::$app->request->getStack($this->owner->id);
             $key = Checksum::calculate($stack);
             \Yii::$app->request->stackField($this->owner->id, \Yii::$app->request->checksumParam, $key);
